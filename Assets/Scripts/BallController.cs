3c77c6507b9d3abc979aa186c9bd3cc234390a3a
Mark <mark.lovesey03@gmail.com>
2025-01-27 16:27:10 -0800

    Refactored input handling

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] float ballSpeed;
+    public void MoveBall(Vector2 input)
+    {
+        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
